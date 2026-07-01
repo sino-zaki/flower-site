8e8c4043aaaa396664ea4f3017b598465fd69a0a
sino-zaki <taiki55110@gmail.com>
2026-07-01 10:43:49 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const fadeItems = document.querySelectorAll('.fade-in');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+}, {
+    threshold: 0.2
+});
+
+fadeItems.forEach((item) => {
+    observer.observe(item);
+});
